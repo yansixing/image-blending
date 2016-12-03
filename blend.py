581db9634d3799f56d3cfba029e5b2f74870e1d4
yanxing <yansixing@qq.com>
2016-12-04 00:10:48 +0800

    Update blend.py

--- blend.py
+++ blend.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
-from __future__ import division
 from PIL import Image
 
 #Input two images
 front = Image.open("images/bright.png")
 back = Image.open("images/dusk.png")
-
-#convert the image to black and white
+#covert to 'L' mode
 front_L = front.convert('L')
 back_L = back.convert('L')
-
-front_L.save("images/front_L.png")
-back_L.save("images/back_L.png")
-
-#get image size
-# width = front_L.size[0]
-# height = front_L.size[1]
 
 front_RGBA = front.convert('RGBA')
 back_RGBA = back.convert('RGBA')
@@ -23,7 +14,7 @@ back_RGBA = back.convert('RGBA')
 front_L = front_L.getdata()
 back_L = back_L.getdata()
 
-def deleteFrontPixel(data,threshold):
+def frontHandle(data,threshold):
 	newData = []
 	for item in data:
 		if item > threshold:
@@ -32,7 +23,7 @@ def deleteFrontPixel(data,threshold):
 			newData.append((item,item,item,255))
 	return newData
 
-def deleteBackPixel(data,threshold):
+def backHandle(data,threshold):
 	newData = []
 	for item in data:
 		newData.append((item,item,item,255))
@@ -41,6 +32,7 @@ def deleteBackPixel(data,threshold):
 def imageBlending(front,back):
 	newData = []
 	for index,item in enumerate(front):
+		#core part⬇️
 		frontG = front[index][0]
 		backG = back[index][0]
 		dstAlpha = round(1 - (frontG - backG)/256, 3)
@@ -49,13 +41,11 @@ def imageBlending(front,back):
 		newData.append((dstRGB,dstRGB,dstRGB,dstAlpha))
 	return newData
 
-
-
-frontData = deleteFrontPixel(front_L,224)
+frontData = frontHandle(front_L,224)
 front_RGBA.putdata(frontData)
 front_RGBA.save("images/img.png")
 
-backData = deleteBackPixel(back_L,224)
+backData = backHandle(back_L,160)
 back_RGBA.putdata(backData)
 back_RGBA.save("images/img2.png")
 
@@ -66,9 +56,5 @@ front_RGBA.putdata(img)
 front_RGBA.save("images/img3.png")
 
 
-
-# img = Image.blend(front_RGBA,back_RGBA,0.2)
-# img.save("images/img3.png")
-
-print("\nsuccess.\n")
+print("success.\n")
 # if __name__ == '__main__':
